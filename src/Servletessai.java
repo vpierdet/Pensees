f16426ec569a9896b857bf2b877999f9bc43c3b5
MarineRenard <marinerenard67@gmail.com>
2018-04-12 11:46:42 +0200

    essai_commit_marine

--- src/Servletessai.java
+++ src/Servletessai.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Servletessai")
+@WebServlet(name = "Servletessai_marine")
 public class Servletessai extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
